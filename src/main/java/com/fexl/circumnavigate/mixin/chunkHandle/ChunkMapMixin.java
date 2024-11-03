52f1c05f4edb6160c8c66af99a947076079cafc0
Nathan Benson <87458603+FamroFexl@users.noreply.github.com>
2024-11-03 16:44:04 -0700

    Fixed players blinking in and out when crossing bounds when viewed by other players. This might apply to other entities as well.

--- src/main/java/com/fexl/circumnavigate/mixin/chunkHandle/ChunkMapMixin.java
+++ src/main/java/com/fexl/circumnavigate/mixin/chunkHandle/ChunkMapMixin.java
@@ -2,16 +2,16 @@
 
 package com.fexl.circumnavigate.mixin.chunkHandle;
 
+import com.fexl.circumnavigate.core.WorldTransformer;
 import com.fexl.circumnavigate.storage.TransformerRequests;
-import net.minecraft.server.level.ChunkMap;
-import net.minecraft.server.level.ChunkTrackingView;
-import net.minecraft.server.level.ServerEntity;
-import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.core.SectionPos;
+import net.minecraft.server.level.*;
 import net.minecraft.server.network.ServerPlayerConnection;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.phys.Vec3;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -24,19 +24,25 @@ import java.util.Set;
 
 @Mixin(ChunkMap.class)
 public abstract class ChunkMapMixin {
+	@Final @Shadow public ServerLevel level;
+
 	/**
 	 * Stores the serverLevel for usage further down the call chain where it was not passed.
 	 */
 	@Inject(method = "isChunkTracked", at = @At("HEAD"), cancellable = true)
 	public void isChunkTracked(ServerPlayer player, int x, int z, CallbackInfoReturnable<Boolean> cir) {
 		TransformerRequests.chunkMapLevel = player.serverLevel();
+		WorldTransformer transformer = player.serverLevel().getTransformer();
+		cir.setReturnValue(player.getChunkTrackingView().contains(x, z) && !player.connection.chunkSender.isPending(ChunkPos.asLong(transformer.xTransformer.unwrapChunkFromLimit(player.getClientChunk().x, x), transformer.zTransformer.unwrapChunkFromLimit(player.getClientChunk().z, z))));
 	}
 
 	/**
 	 * Stores the serverLevel for usage further down the call chain where it was not passed.
 	 */
 	@Inject(method = "applyChunkTrackingView", at = @At("HEAD"))
-	public void applyChunkTrackingView(ServerPlayer player, ChunkTrackingView chunkTrackingView, CallbackInfo ci) {
+	public void applyChunkTrackingViewMixin(ServerPlayer player, ChunkTrackingView chunkTrackingView, CallbackInfo ci) {
 		TransformerRequests.chunkMapLevel = player.serverLevel();
 	}
+
+
 }
