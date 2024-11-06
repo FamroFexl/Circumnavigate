c0b57d6ef2c9c47d58ab6e67eb3ab33e0f6f40cd
skidam <skidamdev@proton.me>
2024-11-06 17:53:56 +0100

    vanilla parity fixes

--- src/main/java/com/fexl/circumnavigate/mixin/entityHandle/LivingEntityMixin.java
+++ src/main/java/com/fexl/circumnavigate/mixin/entityHandle/LivingEntityMixin.java
@@ -17,24 +17,30 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
 
-    @Unique private double x;
-    @Unique private double z;
+    @Unique private double deltaX;
+    @Unique private double deltaZ;
 
     @Inject(method = "hurt", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;knockback(DDD)V"))
     public void hurt(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir, @Local(name = "entity2") Entity enemy) {
         Entity thiz = (Entity) (Object) this;
         WorldTransformer transformer = enemy.level().getTransformer();
-        x = transformer.xTransformer.getActualDistanceTo(thiz.getX(), enemy.getX());
-        z = transformer.zTransformer.getActualDistanceTo(thiz.getZ(), enemy.getZ());
+        deltaX = transformer.xTransformer.getActualDistanceTo(thiz.getX(), enemy.getX());
+        deltaZ = transformer.zTransformer.getActualDistanceTo(thiz.getZ(), enemy.getZ());
+
+        // Vanilla parity code
+        while (deltaX * deltaX + deltaZ * deltaZ < 1.0E-4) {
+            deltaX = (Math.random() - Math.random()) * 0.01;
+            deltaZ = (Math.random() - Math.random()) * 0.01;
+        }
     }
 
     @WrapMethod(method = "knockback")
     public void knockback(double strength, double x, double z, Operation<Void> original) {
-        original.call(strength, this.x, this.z);
+        original.call(strength, this.deltaX, this.deltaZ);
     }
 
     @WrapMethod(method = "indicateDamage")
     public void indicateDamage(double xDistance, double zDistance, Operation<Void> original) {
-        original.call(this.x, this.z);
+        original.call(this.deltaX, this.deltaZ);
     }
 }
