515837eafb0052af98a57d63168e2ac4008cf460
Nathan Benson <87458603+FamroFexl@users.noreply.github.com>
2024-11-07 15:33:14 -0700

    Updated light engine changes to not apply client-side

---
+++ src/main/java/com/fexl/circumnavigate/injected/IsClientSideInjector.java
@@ -0,0 +1,9 @@
+/*
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+package com.fexl.circumnavigate.injected;
+
+public interface IsClientSideInjector {
+	boolean isClientSide();
+}
--- src/main/java/com/fexl/circumnavigate/mixin/lightHandle/BlockLightEngineMixin.java
+++ src/main/java/com/fexl/circumnavigate/mixin/lightHandle/BlockLightEngineMixin.java
@@ -3,29 +3,37 @@ package com.fexl.circumnavigate.mixin.lightHandle;
 import com.fexl.circumnavigate.core.WorldTransformer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.lighting.BlockLightEngine;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(BlockLightEngine.class)
 public class BlockLightEngineMixin {
+	@Unique
+	BlockGetter level = ((LightEngineAccessor) this).getChunkSource().getLevel();
+	
     @ModifyVariable(method = "propagateIncrease", at = @At("HEAD"), index = 1, argsOnly = true)
     public long propagateIncrease(long pos) {
-        WorldTransformer transformer = ((LightEngineAccessor) this).getChunkSource().getLevel().getTransformer();
+		if(level.isClientSide()) return pos;
+        WorldTransformer transformer = level.getTransformer();
         return transformer.translateBlockToBounds(pos);
     }
 
     @Redirect(method = "propagateIncrease", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/BlockPos;offset(JLnet/minecraft/core/Direction;)J"))
     public long changeOffset(long pos, Direction direction) {
-        WorldTransformer transformer = ((LightEngineAccessor) this).getChunkSource().getLevel().getTransformer();
+		if(level.isClientSide()) return BlockPos.offset(pos, direction);
+        WorldTransformer transformer = level.getTransformer();
         return transformer.translateBlockToBounds(BlockPos.offset(pos, direction));
     }
 
     @Redirect(method = "propagateDecrease", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/BlockPos;offset(JLnet/minecraft/core/Direction;)J"))
     public long changeOffset2(long pos, Direction direction) {
-        WorldTransformer transformer = ((LightEngineAccessor) this).getChunkSource().getLevel().getTransformer();
+		if(level.isClientSide()) return BlockPos.offset(pos, direction);
+        WorldTransformer transformer = level.getTransformer();
         return transformer.translateBlockToBounds(BlockPos.offset(pos, direction));
     }
 }
