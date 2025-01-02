037b81eb4da3a3cc0a7bedeb861d1d2a9ff141a8
Famro Fexl <87458603+FamroFexl@users.noreply.github.com>
2025-01-02 14:05:48 -0700

    Fixed missing class.

--- src/client/java/com/fexl/circumnavigate/CircumnavigateClient.java
+++ src/client/java/com/fexl/circumnavigate/CircumnavigateClient.java
@@ -3,6 +3,7 @@
 package com.fexl.circumnavigate;
 
 import com.fexl.circumnavigate.client.storage.TransformersStorage;
+import com.fexl.circumnavigate.core.WorldTransformer;
 import com.fexl.circumnavigate.network.packet.ChunkLoadingLevelsPayload;
 import com.fexl.circumnavigate.network.packet.LevelWrappingPayload;
 import net.fabricmc.api.ClientModInitializer;
@@ -20,7 +21,7 @@ public class CircumnavigateClient implements ClientModInitializer {
 	public void onInitializeClient() {
 		//Read an incoming transformer from the server. Received during server configuration.
 		ClientConfigurationNetworking.registerGlobalReceiver(LevelWrappingPayload.TYPE, (((payload, context) -> {
-			TransformersStorage.setTransformer(payload.levelKey(), payload.transformer());
+			TransformersStorage.setTransformer(payload.levelKey(), new WorldTransformer(payload.wrappingSettings(), true));
 		})));
 
 		/**
