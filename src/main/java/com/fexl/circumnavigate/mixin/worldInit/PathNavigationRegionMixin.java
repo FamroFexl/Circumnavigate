593c20274464a2db71e756904e321e3a9e2ac488
Nathan Benson <87458603+FamroFexl@users.noreply.github.com>
2025-01-16 13:45:26 -0700

    Entities now cross bounds properly with wrapped chunks

--- src/main/java/com/fexl/circumnavigate/mixin/worldInit/PathNavigationRegionMixin.java
+++ src/main/java/com/fexl/circumnavigate/mixin/worldInit/PathNavigationRegionMixin.java
@@ -4,13 +4,18 @@
 
 package com.fexl.circumnavigate.mixin.worldInit;
 
+import com.fexl.circumnavigate.core.DimensionTransformer;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.PathNavigationRegion;
+import net.minecraft.world.level.chunk.ChunkSource;
+import net.minecraft.world.level.chunk.LevelChunk;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PathNavigationRegion.class)
@@ -20,5 +25,11 @@ public class PathNavigationRegionMixin {
 		PathNavigationRegion thiz = (PathNavigationRegion) (Object) this;
 
 		thiz.setTransformer(level.getTransformer().onlyServerSide());
+	}
+
+	@Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/chunk/ChunkSource;getChunkNow(II)Lnet/minecraft/world/level/chunk/LevelChunk;"))
+	public LevelChunk init(ChunkSource instance, int chunkX, int chunkZ, @Local(argsOnly = true) Level level) {
+		DimensionTransformer transformer = level.getTransformer().onlyServerSide();
+		return instance.getChunkNow(transformer.Chunk.X.wrapToBounds(chunkX), transformer.Chunk.Z.wrapToBounds(chunkZ));
 	}
 }
