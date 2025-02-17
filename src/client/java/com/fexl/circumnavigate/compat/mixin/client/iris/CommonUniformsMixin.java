6b45107650f437c4e4d2e78365014c3e0ddb2f18
Famro Fexl <87458603+FamroFexl@users.noreply.github.com>
2025-02-16 18:53:15 -0700

    Added `dimensionBounds` uniform to Iris

--- src/client/java/com/fexl/circumnavigate/compat/mixin/client/iris/CommonUniformsMixin.java
+++ src/client/java/com/fexl/circumnavigate/compat/mixin/client/iris/CommonUniformsMixin.java
@@ -1,9 +1,13 @@
 package com.fexl.circumnavigate.compat.mixin.client.iris;
 
+import com.fexl.circumnavigate.core.DimensionTransformer;
 import net.irisshaders.iris.gl.uniform.UniformHolder;
+import net.irisshaders.iris.gl.uniform.UniformUpdateFrequency;
 import net.irisshaders.iris.shaderpack.properties.PackDirectives;
 import net.irisshaders.iris.uniforms.CommonUniforms;
 import net.irisshaders.iris.uniforms.FrameUpdateNotifier;
+import net.minecraft.client.Minecraft;
+import org.joml.Vector2i;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,5 +17,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class CommonUniformsMixin {
     @Inject(method = "generalCommonUniforms", at = @At("HEAD"), remap = false)
     private static void general(UniformHolder uniforms, FrameUpdateNotifier updateNotifier, PackDirectives directives, CallbackInfo ci) {
+        uniforms.uniform2i(UniformUpdateFrequency.PER_FRAME, "dimensionBounds", CommonUniformsMixin::getDimensionBounds);
+    }
+
+    private static Vector2i getDimensionBounds() {
+        DimensionTransformer transformer = Minecraft.getInstance().level.getTransformer();
+        return new Vector2i(transformer.xWidth, transformer.zWidth);
     }
 }
